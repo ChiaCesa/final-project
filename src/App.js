1f2ccea0337abbfe6fcc5f485f6b60d4c36e50f1
Chiara <kia.cesa@gmail.com>
2021-05-30 18:03:48 +0200

    js+css

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="box">
+          <div className="row">
+<div className="col">
+  <h1>What do you need?</h1>
+</div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <a className="weatherApp" href="https://determined-nobel-73068d.netlify.app" target="_blank" rel="noreferrer">Weather</a>
+            </div>
+            <div className="col-6">
+              <a  className="dictionaryApp" href="https://youthful-lovelace-bcc60e.netlify.app" target="_blank" rel="noreferrer">Dictionary</a>
+            </div>
+            
+          </div>
+          </div>
+       <img src="./img/catBanner.jpg" alt="catBanner"/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
